af865434a5c85752a003e966085ddc5108a8149b
Alessandro <defranceschi_a@yahoo.com>
2025-10-08 18:49:29 +0200

    fix: correct paths in package.json and add outExtension function in tsup.config.ts

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['esm', 'cjs'],
+  outExtension({ format }) {
+    return {
+      js: format === 'esm' ? '.mjs' : '.cjs'
+    };
+  },
   dts: true,
   sourcemap: true,
   clean: true,
